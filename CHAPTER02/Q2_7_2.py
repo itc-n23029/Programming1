30ad71964d5b21ef82f37f7ef63f6c49b94f0fae
Yuya Miyagi <n23029@std.it-college.ac.jp>
2023-07-06 07:24:51 +0000

    2.5~7

---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,9 @@
+# 昇順
+my_list = ["orange", "apple", "grape", "banana"]
+sorted_list = sorted(my_list)
+print(sorted_list)
+
+# 降順
+my_list = ["orange", "apple", "grape", "banana"]
+sorted_list = sorted(my_list, reverse=True)
+print(sorted_list)
---
+++ CHAPTER02/Q2_8_1.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+reverse_even = [num for num in my_list[::-1] if num % 2 == 0]
+print(reverse_even)
