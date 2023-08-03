a6dc0cef493c14b8012a1305cf77061c301b9fd8
Yuya Miyagi <n23029@std.it-college.ac.jp>
2023-08-03 06:17:55 +0000

    5_1~7_1

---
+++ CHAPTER04/Q4_6_2.py
@@ -0,0 +1,27 @@
+import random
+
+
+def generate_students_data(num_students=10):
+    students_data = []
+    for i in range(num_students):
+        name = "n" + str(random.randint(10, 50))
+        height = random.randint(150, 190)
+        weight = random.randint(50, 80)
+        students_data.append((name, height, weight))
+    return students_data
+
+
+students_data = generate_students_data(10)
+students_data
+
+students_by_height = sorted(students_data, key=lambda s: s[1])
+students_by_weight = sorted(students_data, key=lambda s: s[2])
+
+print("\nSort by height")
+for student in students_by_height:
+    print(student)
+
+
+print("\nSort by weight")
+for student in students_by_weight:
+    print(student)
